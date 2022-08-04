22f8d3c2a6a5b05cdb1f5a4f93ad5b8d7f7bd128
Jamie Dalan Cox <74336097+dalancox@users.noreply.github.com>
2022-08-04 12:25:12 -0600

    Trying new things

--- index.js
+++ index.js
@@ -5,12 +5,50 @@ console.log('Initialized...');
 const input = document.getElementById('search-bar');
 const userCardTemplate = document.querySelector("[data-user-template]")
 const userCardContainer = document.querySelector("[data-user-cards-container]")
+const btn = document.getElementById("submit")
+const loader = document.getElementById("loading");
 
 // event listeners for updating the value
 input.addEventListener('input', updateValue);
 
 // users array for filtering 
 let users = [];
+
+btn.addEventListener("click", fetchHandler);
+
+function displayLoading() {
+    loader.classList.add("display");
+    // to stop loading after some time
+    setTimeout(() => {
+        loader.classList.remove("display");
+    }, 5000);
+}
+
+function hideLoading() {
+    loader.classList.remove("display");
+}
+
+function fetchHandler(){
+
+    displayLoading();
+
+    // fetch API to grab dummy data
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res => res.json())
+        .then(data => {
+            hideLoading();
+
+            users = data.map(user => {
+                const card = userCardTemplate.content.cloneNode(true).children[0];
+                const header = card.querySelector('[data-header]');
+                const body = card.querySelector('[data-body]');
+                header.textContent = user.name;
+                body.textContent = user.email;
+                userCardContainer.append(card)
+                return { name: user.name, email: user.email, element: card }
+            });
+        });
+    }
 
 // updating DOM with filtered list 
 function updateValue(e) {
@@ -19,21 +57,7 @@ function updateValue(e) {
         const isVisble = 
             user.name.toLowerCase().includes(value) ||
             user.email.toLowerCase().includes(value);
-        user.element.classList.toggle("hide", !isVisble);
+            user.element.classList.toggle("hide", !isVisble);
     })
 }
 
-// fetch API to grab dummy data
-fetch('https://jsonplaceholder.typicode.com/users')
-    .then(res => res.json())
-    .then(data => {
-        users = data.map(user => {
-            const card = userCardTemplate.content.cloneNode(true).children[0];
-            const header = card.querySelector('[data-header]');
-            const body = card.querySelector('[data-body]');
-            header.textContent = user.name;
-            body.textContent = user.email;
-            userCardContainer.append(card)
-            return { name: user.name, email: user.email, element: card }
-        });
-    });
